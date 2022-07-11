27dbcc7a4b0f4a8782cfa57dd367d86ddfbf25fe
porwaltz <38365041+porwaltz@users.noreply.github.com>
2022-07-11 19:18:10 -0300

    vimmit

--- investigacion-uba/pages/index.js
+++ investigacion-uba/pages/index.js
@@ -5,11 +5,16 @@ import { Box, Flex, Spacer, AspectRatio, Center } from "@chakra-ui/react"
 
 export default function Home() {
   return (
-    <Center bg='teal'height='fit-content'>
-      <Box className="iframe" as="iframe" bg='red' width='720px' m='auto' scrolling="no"
-      
+    <AspectRatio>
+      <iframe scrolling='no' m='auto' width="720px"
       src="https://docs.google.com/document/d/e/2PACX-1vTEi648s7U5jNcQza9yGkVsYefIYppovOfekibvQIZlkvZJboJDewFpSr1hHCWavCdwH3doFpCCjzw1/pub?embedded=true">
-      </Box>
-    </Center>
+      </iframe>
+    </AspectRatio>
   )
 }
+
+{/* <Box className="iframe" as="iframe" bg='teal' 
+      w='720px' m='auto' scrolling="no"
+      h="100%" border='none' pos='relative'
+      src="https://docs.google.com/document/d/e/2PACX-1vTEi648s7U5jNcQza9yGkVsYefIYppovOfekibvQIZlkvZJboJDewFpSr1hHCWavCdwH3doFpCCjzw1/pub?embedded=true">
+      </Box> */}
